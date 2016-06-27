2855674ac73c4d98b01bdbe9a692eb62afceae3c
Josh Forman <jforman@outbrain.com>
2016-06-27 16:15:16 -0400

    Updated description

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(name='torch',
       version='0.0',
-      description="Prometheus Agent",
-      long_description="""A Prometheus agent which registers with Consul for discovery purposes""",
+      description="Prometheus aggregator",
+      long_description="""A Prometheus aggregator which registers with Consul for discovery purposes""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       author='Joshua Forman',
       author_email='jforman@outbrain.com',
